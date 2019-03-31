c5f20db16691b7b8bb62d80446ba96a94158a39c
Viridiana Romero <31528909+viritaromero@users.noreply.github.com>
2019-03-31 00:24:01 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1AsTBEt60V1EH2ZMRGCs0AWBO7MwVUyTW'
+export_file_name = 'export2.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Anger and Violence', 'Anxiety and Depression', 'Happiness']
 path = Path(__file__).parent
 
 app = Starlette()
